ac0777a425741839533e73685c41c11f26b8b544
Joe <joseph.krensky@gmail.com>
2019-08-23 12:04:40 -0700

    Master / detail components added
    Still clappin them cheeks

--- angular/src/app/hero-detail/hero-detail.component.ts
+++ angular/src/app/hero-detail/hero-detail.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styles: []
 })
 export class HeroDetailComponent implements OnInit {
-
+  @Input() hero: Hero;
   constructor() { }
 
   ngOnInit() {
