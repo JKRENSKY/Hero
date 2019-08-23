b61e4fed3ab6e1d68ee74b40d90cc1caca74e5f9
Joe K <36493556+JKRENSKY@users.noreply.github.com>
2019-08-23 09:24:46 -0700

    Hero Project Files
    Work split between laptop and pc stations, check for issues w service host
    
    current guide portion:
     https://angular.io/tutorial/toh-pt3

---
+++ angular/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styles: []
+})
+export class HeroDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ angular/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Nicolas Cage' },
+  { id: 12, name: 'Nicolas Cage' },
+  { id: 13, name: 'Nicolas Cage' },
+  { id: 14, name: 'Nicolas Cage' },
+  { id: 15, name: 'Nicolas Cage' },
+  { id: 16, name: 'Nicolas Cage' },
+  { id: 17, name: 'Nicolas Cage' },
+  { id: 18, name: 'Nicolas Cage' },
+  { id: 19, name: 'Nicolas Cage' },
+  { id: 20, name: 'The Juggernaut' }
+];
